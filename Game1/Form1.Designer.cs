264b9d3944063134fa8b52575505e284b2e9ce6a
mdnabeel <md.nabeel786@gmail.com>
2020-10-28 22:53:09 +0530

    Add project files.

---
+++ Game1/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Game1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.rectangleShape9 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rectangleShape8 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rectangleShape7 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rectangleShape6 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rectangleShape5 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rectangleShape4 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rectangleShape3 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rectangleShape2 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape9,
+            this.rectangleShape8,
+            this.rectangleShape7,
+            this.rectangleShape6,
+            this.rectangleShape5,
+            this.rectangleShape4,
+            this.rectangleShape3,
+            this.rectangleShape2,
+            this.rectangleShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(586, 308);
+            this.shapeContainer1.TabIndex = 0;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // rectangleShape9
+            // 
+            this.rectangleShape9.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.rectangleShape9.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape9.Location = new System.Drawing.Point(67, 286);
+            this.rectangleShape9.Name = "rectangleShape9";
+            this.rectangleShape9.Size = new System.Drawing.Size(82, 2);
+            this.rectangleShape9.Click += new System.EventHandler(this.rectangleShape5_Click);
+            // 
+            // rectangleShape8
+            // 
+            this.rectangleShape8.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.rectangleShape8.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape8.Location = new System.Drawing.Point(69, 274);
+            this.rectangleShape8.Name = "rectangleShape8";
+            this.rectangleShape8.Size = new System.Drawing.Size(82, 2);
+            this.rectangleShape8.Click += new System.EventHandler(this.rectangleShape5_Click);
+            // 
+            // rectangleShape7
+            // 
+            this.rectangleShape7.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.rectangleShape7.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape7.Location = new System.Drawing.Point(68, 260);
+            this.rectangleShape7.Name = "rectangleShape7";
+            this.rectangleShape7.Size = new System.Drawing.Size(82, 2);
+            this.rectangleShape7.Click += new System.EventHandler(this.rectangleShape5_Click);
+            // 
+            // rectangleShape6
+            // 
+            this.rectangleShape6.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.rectangleShape6.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape6.Location = new System.Drawing.Point(67, 247);
+            this.rectangleShape6.Name = "rectangleShape6";
+            this.rectangleShape6.Size = new System.Drawing.Size(82, 2);
+            this.rectangleShape6.Click += new System.EventHandler(this.rectangleShape5_Click);
+            // 
+            // rectangleShape5
+            // 
+            this.rectangleShape5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.rectangleShape5.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape5.Location = new System.Drawing.Point(70, 143);
+            this.rectangleShape5.Name = "rectangleShape5";
+            this.rectangleShape5.Size = new System.Drawing.Size(82, 2);
+            this.rectangleShape5.Click += new System.EventHandler(this.rectangleShape5_Click);
+            // 
+            // rectangleShape4
+            // 
+            this.rectangleShape4.BackColor = System.Drawing.Color.Red;
+            this.rectangleShape4.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape4.Location = new System.Drawing.Point(549, 36);
+            this.rectangleShape4.Name = "rectangleShape4";
+            this.rectangleShape4.Size = new System.Drawing.Size(7, 13);
+            this.rectangleShape4.Click += new System.EventHandler(this.rectangleShape4_Click);
+            // 
+            // rectangleShape3
+            // 
+            this.rectangleShape3.BackColor = System.Drawing.Color.Cyan;
+            this.rectangleShape3.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape3.Location = new System.Drawing.Point(556, 25);
+            this.rectangleShape3.Name = "rectangleShape3";
+            this.rectangleShape3.Size = new System.Drawing.Size(8, 34);
+            this.rectangleShape3.Click += new System.EventHandler(this.rectangleShape3_Click);
+            // 
+            // rectangleShape2
+            // 
+            this.rectangleShape2.BackColor = System.Drawing.Color.Lime;
+            this.rectangleShape2.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape2.Location = new System.Drawing.Point(564, 16);
+            this.rectangleShape2.Name = "rectangleShape2";
+            this.rectangleShape2.Size = new System.Drawing.Size(9, 53);
+            this.rectangleShape2.Click += new System.EventHandler(this.rectangleShape2_Click);
+            // 
+            // rectangleShape1
+            // 
+            this.rectangleShape1.BackColor = System.Drawing.Color.Yellow;
+            this.rectangleShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape1.Location = new System.Drawing.Point(573, 1);
+            this.rectangleShape1.Name = "rectangleShape1";
+            this.rectangleShape1.Size = new System.Drawing.Size(10, 84);
+            this.rectangleShape1.Click += new System.EventHandler(this.rectangleShape1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Shoot";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(586, 308);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.shapeContainer1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.Location = new System.Drawing.Point(0, -1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer2;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape4;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape3;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape2;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape5;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape9;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape8;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape7;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape6;
+        private System.Windows.Forms.Timer timer3;
+
+    }
+}
+
---
+++ Game1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Game1
+{
+    public partial class Form1 : Form
+    {
+        int i = 5;
+
+        public Form1()
+        {
+          
+            InitializeComponent();
+            timer1.Start();
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            rectangleShape1.Location = new Point(rectangleShape1.Location.X, rectangleShape1.Location.Y + 5);
+            if (rectangleShape1.Location.Y + 100 > this.Height)
+            {
+                //ovalShape1.Location = new Point(ovalShape1.Location.X,0-ovalShape1.Height);
+                rectangleShape1.Location = new Point(573, 1);
+            }
+
+
+            rectangleShape2.Location = new Point(rectangleShape2.Location.X, rectangleShape2.Location.Y + 5);
+            if (rectangleShape2.Location.Y + 85 > this.Height)
+            {
+                //ovalShape2.Location = new Point(ovalShape2.Location.X, 0-ovalShape2.Height);
+                rectangleShape2.Location = new Point(564, 16);
+            }
+
+
+            rectangleShape3.Location = new Point(rectangleShape3.Location.X, rectangleShape3.Location.Y + 5);
+            if (rectangleShape3.Location.Y + 75 > this.Height)
+            {
+                //ovalShape3.Location = new Point(ovalShape3.Location.X, 0 - ovalShape3.Height);
+                rectangleShape3.Location = new Point(556, 25);
+            }
+
+
+            rectangleShape4.Location = new Point(rectangleShape4.Location.X, rectangleShape4.Location.Y + 5);
+            if (rectangleShape4.Location.Y + 65 > this.Height)
+            {
+                //ovalShape4.Location = new Point(ovalShape4.Location.X, 0 - ovalShape4.Height);
+                rectangleShape4.Location = new Point(549, 36);
+            }
+
+        }
+
+
+       
+
+     
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {   
+                timer2.Start();
+              
+        }
+
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+           
+
+            rectangleShape5.Location = new Point(rectangleShape5.Location.X + 5, rectangleShape5.Location.Y);
+
+            // if (rectangleShape5.Bounds.IntersectsWith(rectangleShape1.Bounds))
+            // {
+            //rectangleShape5.Location = new Point(70, 143);
+            // timer2.Stop();
+            //}
+            if ((rectangleShape5.Bounds.IntersectsWith(rectangleShape1.Bounds)) || (rectangleShape5.Bounds.IntersectsWith(rectangleShape2.Bounds)) || (rectangleShape5.Bounds.IntersectsWith(rectangleShape3.Bounds)) || (rectangleShape5.Bounds.IntersectsWith(rectangleShape4.Bounds)) || (rectangleShape5.Location.X >this.Width))
+            {
+                
+               
+                            timer1.Stop();
+                            timer2.Stop();
+                            Thread.Sleep(5000);
+                            timer1.Start();
+                            rectangleShape5.Location = new Point(70, 143);
+                            
+                            i--;
+                            if (i == 4)
+                            {
+                                
+                                rectangleShape6.Hide();
+                            }
+                            if (i == 3)
+                            {
+                               
+                                rectangleShape7.Hide();
+                            }
+                            if (i == 2)
+                            {
+                                
+                                rectangleShape8.Hide();
+                            }
+                            if (i == 1)
+                            {
+                                
+                                rectangleShape9.Hide();
+                            }
+                            if (i == 0)
+                            {
+
+                                rectangleShape5.Hide();
+                            }
+                            //rectangleShape6.Location = new Point(70, 143);
+                            //rectangleShape9.Hide();
+
+
+                        }
+
+                    }
+                
+           
+        
+
+
+
+
+
+
+        private void rectangleShape1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rectangleShape2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rectangleShape3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rectangleShape4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rectangleShape5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+
+       
+
+
+    }
+}
+
+
+
